c977442240c678810e1d339e3cc2037d3f926daf
OneEvil <oneevil@bk.ru>
2019-05-02 15:02:23 +0000

    Update count records

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 wget -O dump.csv https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv
 cut -d ";" -f1 dump.csv | tr '|' '\n' | tr -d ' ' | sort -t. -n -k1,1 -k2,2 -k3,3 -k4,4 | uniq | tail -n +3 > rkn_list.txt
-python3 net_list.py rkn_list.txt 35000 | sort -t. -n -k1,1 -k2,2 -k3,3 -k4,4 > rkn_subnets.txt
+python3 net_list.py rkn_list.txt 65500 | sort -t. -n -k1,1 -k2,2 -k3,3 -k4,4 > rkn_subnets.txt
 cat rkn_subnets.txt | awk '{ print "add address="$0" list=rkn" }' > subnets.rsc
 sed -i -e '1 s/^/\/ip firewall address-list\n/;' subnets.rsc
 cat rkn_subnets.txt | awk '{ print "ipset add rkn address "$0 }' > subnets_ipset.rsc
